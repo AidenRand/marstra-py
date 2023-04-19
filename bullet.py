25ff83537f39e617a3bc0ddb1dbf696895b4ab9d
aiden <aidenrphotos@gmail.com>
2023-04-19 12:07:22 -0400

    Fire bullets when space bar is pressed

--- bullet.py
+++ bullet.py
@@ -1,20 +1,16 @@
 import pygame
 
 
-class Bullet:
-    def __init__(self, screen, color, width, height, x, y, y_speed):
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, screen, x, y, y_speed):
+        pygame.sprite.Sprite.__init__(self)
         self.screen = screen
-        self.color = color
-        self.width = width
-        self.height = height
         self.x = x
         self.y = y
         self.y_speed = y_speed
-        self.bullet = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.image = pygame.image.load("assets/bullet.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
 
-    def move_bullet(self, fire_bullet):
-        if fire_bullet:
-            self.bullet.y -= self.y_speed
-
-    def draw_bullet(self):
-        pygame.draw.rect(self.screen, self.color, self.bullet)
+    def update(self):
+        self.rect.y -= self.y_speed
--- main.py
+++ main.py
@@ -9,9 +9,6 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("STARSTRA")
 
-bg_img = pygame.image.load("assets/background.png")
-bg_img = pygame.transform.scale(bg_img, (screen_width, screen_height))
-
 
 purple = (168, 103, 205)
 green = (87, 147, 50)
@@ -20,7 +17,8 @@ white = (200, 200, 200)
 x = 460
 y = 700
 new_player = player.Player(screen, "assets/player.png", x, y, 7)
-new_bullet = bullet.Bullet(screen, white, 5, 10, x, y, 10)
+
+bullet_group = pygame.sprite.Group()
 
 while True:
     for event in pygame.event.get():
@@ -28,12 +26,13 @@ while True:
             pygame.quit()
 
     screen.fill((5, 5, 5))
-    screen.blit(bg_img, (0, 0))
     new_player.wall_collision()
     new_player.update()
     key_input = pygame.key.get_pressed()
     if key_input[pygame.K_SPACE]:
-        new_bullet.move_bullet(True)
-    new_bullet.draw_bullet()
-    clock.tick(60)
+        new_bullet = bullet.Bullet(screen, x, y, 10)
+        bullet_group.add(new_bullet)
+    bullet_group.draw(screen)
+    bullet_group.update()
+    clock.tick(30)
     pygame.display.flip()
